7ee69bedee8c1a4e1b34e0f2b5046de2533802b0
afghanelyas <afghan.elyas1@gmail.com>
2023-06-06 19:07:17 +0430

    set some issues

--- app/src/main/java/com/example/myapplication/DoctorDetailsActivity.java
+++ app/src/main/java/com/example/myapplication/DoctorDetailsActivity.java
@@ -2,13 +2,34 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DoctorDetailsActivity extends AppCompatActivity {
-
+    TextView tv;
+    Button btn;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_details);
+        tv = findViewById(R.id.textViewDDTitle);
+        btn = findViewById(R.id.buttonDDBack);
+
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        tv.setText(title);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DoctorDetailsActivity.this , FindDoctorActivity.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -31,33 +31,34 @@ public class LoginActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+    startActivity(new Intent(LoginActivity.this, HomeActivity.class));
                 //check the username and password empty
 
                 String username = edUsername.getText().toString();
                 String password = edPassword.getText().toString();
                 Database db = new Database(getApplicationContext() , "healthcare" , null , 1);
-                if(username.length() == 0 || password.length() == 0){
-                    Toast.makeText(getApplicationContext(), "Please fill all details" , Toast.LENGTH_SHORT).show();
-                }
-                else{
 
-                    if(db.login(username , password) == 1){
-                        Toast.makeText(getApplicationContext() , "Login Success" , Toast.LENGTH_SHORT).show();
-                        SharedPreferences sharedPreferences = getSharedPreferences("share_prefs" , Context.MODE_PRIVATE);
-                        SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.putString("username" , username);
-//                       to save out data with key and value
-                        editor.apply();
-                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
-
-                    }else {
-                        Toast.makeText(getApplicationContext() , "Invalid Username and Password" , Toast.LENGTH_SHORT).show();
-
-                    }
-
-
-                }
+//                if(username.length() == 0 || password.length() == 0){
+//                    Toast.makeText(getApplicationContext(), "Please fill all details" , Toast.LENGTH_SHORT).show();
+//                }
+//                else{
+//
+//                    if(db.login(username , password) == 1){
+//                        Toast.makeText(getApplicationContext() , "Login Success" , Toast.LENGTH_SHORT).show();
+//                        SharedPreferences sharedPreferences = getSharedPreferences("share_prefs" , Context.MODE_PRIVATE);
+//                        SharedPreferences.Editor editor = sharedPreferences.edit();
+//                        editor.putString("username" , username);
+////                       to save out data with key and value
+//                        editor.apply();
+//                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+//
+//                    }else {
+//                        Toast.makeText(getApplicationContext() , "Invalid Username and Password" , Toast.LENGTH_SHORT).show();
+//
+//                    }
+//
+//
+//                }
 
             }
         });
