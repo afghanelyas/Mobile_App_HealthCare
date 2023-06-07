fc5c36cbf22822fbfc3488891ea524aea24bc7a9
afghanelyas <afghan.elyas1@gmail.com>
2023-06-07 18:01:57 +0430

    List view add in FindDoctor activity

--- app/src/main/java/com/example/myapplication/DoctorDetailsActivity.java
+++ app/src/main/java/com/example/myapplication/DoctorDetailsActivity.java
@@ -7,10 +7,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DoctorDetailsActivity extends AppCompatActivity {
     private String[][] doctor_details1 = {
@@ -44,7 +46,8 @@ public class DoctorDetailsActivity extends AppCompatActivity {
     };
     TextView tv;
     Button btn;
-    String[][] Doctor_details = {};
+    String[][] doctor_details = {};
+    HashMap<String , String> item;
     ArrayList list;
     SimpleAdapter sa;
     @SuppressLint("MissingInflatedId")
@@ -61,15 +64,15 @@ public class DoctorDetailsActivity extends AppCompatActivity {
         tv.setText(title);
 
         if(title.compareTo("Family Physicians") == 0){
-            String[][] doctor_details = doctor_details1;
+            doctor_details = doctor_details1;
         }else if(title.compareTo("Dietician") == 0){
-            String[][] doctor_details = doctor_details2;
+            doctor_details = doctor_details2;
         }else if(title.compareTo("Dentist") == 0){
-            String[][] doctor_details = doctor_details3;
+            doctor_details = doctor_details3;
         }else if(title.compareTo("Surgen") == 0){
-            String[][] doctor_details = doctor_details4;
+            doctor_details = doctor_details4;
         }else {
-            String[][] doctor_details = doctor_details5;
+            doctor_details = doctor_details5;
         }
 
         btn.setOnClickListener(new View.OnClickListener() {
@@ -79,6 +82,23 @@ public class DoctorDetailsActivity extends AppCompatActivity {
             }
         });
     list = new ArrayList();
+    for (int i = 0 ; i < doctor_details.length; i++){
+        item = new HashMap<String , String>();
+        item.put("line1" , doctor_details[i][0]);
+        item.put("line2" , doctor_details[i][1]);
+        item.put("line3" , doctor_details[i][2]);
+        item.put("line4" , doctor_details[i][3]);
+        item.put("line5" , "Cons Fees:"+ doctor_details[i][4]+"$");
+        list.add(item);
+    }
 
+//    simple adapter object
+        sa = new SimpleAdapter(this,list,
+                R.layout.multi_lines,
+                new String[]{"line1" , "line2" , "line3" , "line4" , "line5"},
+                new int[]{R.id.line_a, R.id.line_b , R.id.line_c , R.id.line_d , R.id.line_e}
+                );
+        ListView lst = findViewById(R.id.listviewDD);
+        lst.setAdapter(sa);
     }
 }
