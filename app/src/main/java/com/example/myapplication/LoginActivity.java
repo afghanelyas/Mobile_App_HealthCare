1e038732ad58ff1bbe1fc76be43d8e30607321ee
afghanelyas <afghan.elyas1@gmail.com>
2023-06-11 18:40:18 +0430

    done the project

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,12 +2,15 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -28,34 +31,33 @@ public class LoginActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
-                //check the username and password empty
+//                check the username and password empty
 
-//                String username = edUsername.getText().toString();
-//                String password = edPassword.getText().toString();
-//                Database db = new Database(getApplicationContext() , "healthcare" , null , 1);
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                Database db = new Database(getApplicationContext() , "healthcare" , null , 1);
 
-//                if(username.length() == 0 || password.length() == 0){
-//                    Toast.makeText(getApplicationContext(), "Please fill all details" , Toast.LENGTH_SHORT).show();
-//                }
-//                else{
-//
-//                    if(db.login(username , password) == 1){
-//                        Toast.makeText(getApplicationContext() , "Login Success" , Toast.LENGTH_SHORT).show();
-//                        SharedPreferences sharedPreferences = getSharedPreferences("share_prefs" , Context.MODE_PRIVATE);
-//                        SharedPreferences.Editor editor = sharedPreferences.edit();
-//                        editor.putString("username" , username);
-////                       to save out data with key and value
-//                        editor.apply();
-//                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
-//
-//                    }else {
-//                        Toast.makeText(getApplicationContext() , "Invalid Username and Password" , Toast.LENGTH_SHORT).show();
-//
-//                    }
-//
-//
-//                }
+                if(username.length() == 0 || password.length() == 0){
+                    Toast.makeText(getApplicationContext(), "Please fill all details" , Toast.LENGTH_SHORT).show();
+                }
+                else{
+
+                    if(db.login(username , password) == 1){
+                        Toast.makeText(getApplicationContext() , "Login Success" , Toast.LENGTH_SHORT).show();
+                        SharedPreferences sharedPreferences = getSharedPreferences("share_prefs" , Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("username" , username);
+//                       to save out data with key and value
+                        editor.apply();
+                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+
+                    }else {
+                        Toast.makeText(getApplicationContext() , "Invalid Username and Password" , Toast.LENGTH_SHORT).show();
+
+                    }
+
+
+                }
 
             }
         });
